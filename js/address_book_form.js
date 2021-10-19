22f7c5d467f3641b4805adf0cd4d33b1bb1dc7eb
srinadh1304 <srinadh1304@gmail.com>
2021-10-19 23:32:33 +0530

    ADDED: adding entry into local storage

--- js/address_book_form.js
+++ js/address_book_form.js
@@ -5,6 +5,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
     validateZipcode();
 });
 
+let addressBookContactJSONObject = {};
 const validateName = () => {
     const name = document.querySelector("#name");
     name.addEventListener("input", function() {
@@ -69,24 +70,90 @@ const validateZipcode = () => {
     });
 };
 
-function save() {
-    let contact = new Contact();
-    contact.id = new Date().getTime();
-    contact.name = getInputValueById("#name");
-    contact.phoneNumber = getInputValueById("#phoneNumber");
-    contact.address = getInputValueById("#address");
-    contact.city = getInputValueById("#city");
-    contact.state = getInputValueById("#state");
-    contact.zip = getInputValueById("#zip");
-    console.log(contact.toString());
+const save = (event) => {
+    try {
+        setAddressBookContactJSONObject();
+        UpdateLocalStorage();
+        window.location.replace(site_properties.home_page);
+    } catch (submitError) {
+        alert(submitError);
+        return;
+    }
+};
+
+const UpdateLocalStorage = () => {
+    let addressBookList = JSON.parse(localStorage.getItem("AddressBookList"));
+    if (addressBookList) {
+        let contactData = addressBookList.find(contact => contact._id == addressBookContactJSONObject._id);
+        if (!contactData) {
+            addressBookList.push(createAddressBookContactData());
+        } else {
+            const index = addressBookList.map(contact => contact._id).indexOf(contactData._id);
+            addressBookList.splice(index, 1, createAddressBookContactData(contactData._id));
+        }
+    } else {
+        addressBookList = [createAddressBookContactData()];
+    }
+    alert("Local Storage Updated Successfully!\nTotal Contacts : " + addressBookList.length);
+    localStorage.setItem("AddressBookList", JSON.stringify(addressBookList));
 }
+
+const setAddressBookContactJSONObject = () => {
+    addressBookContactJSONObject._name = getValue('#name');
+    addressBookContactJSONObject._address = getValue('#address');
+    addressBookContactJSONObject._phoneNumber = getValue('#phoneNumber');
+    addressBookContactJSONObject._city = getValue('#city');
+    addressBookContactJSONObject._state = getValue('#state');
+    addressBookContactJSONObject._zip = getValue('#zip');
+};
+
+
+const createAddressBookContactData = (id) => {
+    let contactData = new Contact();
+    if (!id) contactData.id = createNewContactId();
+    else contactData.id = id;
+    setAddressBookContactClassObject(contactData);
+    return contactData;
+}
+
+const setAddressBookContactClassObject = (contactData) => {
+    try {
+        contactData.name = addressBookContactJSONObject._name;
+    } catch (error) {
+        setTextValue(".name-error", error);
+        throw error;
+    }
+    try {
+        contactData.address = addressBookContactJSONObject._address;
+    } catch (error) {
+        setTextValue(".address-error", error);
+        throw error;
+    }
+    try {
+        contactData.phoneNumber = addressBookContactJSONObject._phoneNumber;
+    } catch (error) {
+        setTextValue(".tel-error", error);
+        throw error;
+    }
+    contactData.city = addressBookContactJSONObject._city;
+    contactData.state = addressBookContactJSONObject._state;
+    contactData.zip = addressBookContactJSONObject._zip;
+    alert("Added contact   :\n" + contactData.toString());
+};
+
+const createNewContactId = () => {
+    let contactId = localStorage.getItem("ContactID");
+    contactId = !contactId ? 1 : (parseInt(contactId) + 1).toString();
+    localStorage.setItem("ContactID", contactId);
+    return contactId;
+}
+
+const getValue = (propertyId) => {
+    let value = document.querySelector(propertyId).value;
+    return value;
+};
 
 const setTextValue = (id, value) => {
     const element = document.querySelector(id);
     element.textContent = value;
 };
-
-function getInputValueById(property) {
-    let value = document.querySelector(property).value;
-    return value;
-}
