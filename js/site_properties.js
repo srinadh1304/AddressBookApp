25c713df312165c158432c17f32c3fb3d11362db
srinadh1304 <srinadh1304@gmail.com>
2021-10-20 23:25:08 +0530

    REFACTORED: use local storage is set to false

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,5 +1,5 @@
 let site_properties = {
-    use_local_storage: "true",
+    use_local_storage: "false",
     server_url: "http://localhost:3000/ContactDB/",
     home_page: "../pages/home.html",
     add_contact_page: "../pages/address_book_form.html"
