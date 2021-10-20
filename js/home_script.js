c41ed2328c81c9c7915c498389843e4f7220e2f7
srinadh1304 <srinadh1304@gmail.com>
2021-10-20 20:32:16 +0530

    ADDED: ability to view persons data in table

---
+++ js/home_script.js
@@ -0,0 +1,41 @@
+let addressBookContactList;
+window.addEventListener("DOMContentLoaded", () => {
+    addressBookContactList = getAddressBookContactListFromStorage();
+    document.querySelector(".person-count").textContent = addressBookContactList.length;
+    createInnerHtml();
+    localStorage.removeItem("PersonToEdit");
+});
+
+const getAddressBookContactListFromStorage = () => {
+    return localStorage.getItem("AddressBookList") ?
+        JSON.parse(localStorage.getItem("AddressBookList")) : [];
+};
+
+const createInnerHtml = () => {
+    const headerHtml =
+        "<th>Fullname</th>" +
+        "<th>Address</th>" +
+        "<th>City</th>" +
+        "<th>State</th>" +
+        "<th>Zip Code</th>" +
+        "<th>Phone Number</th>" +
+        "<th></th>";
+    let innerHtml = `${headerHtml}`;
+    for (let contactData of addressBookContactList) {
+        innerHtml = `${innerHtml}
+        <tr>
+            <td>${contactData._name}</td>
+            <td>${contactData._address}</td>
+            <td>${contactData._city}</td>
+            <td>${contactData._state}</td>
+            <td>${contactData._zip}</td>
+            <td>${contactData._phoneNumber}</td>
+            <td>
+                <img id="${contactData._id}" onclick="remove(this)" alt="delete" src="../assets/delete-black-18dp.svg">
+                <img id="${contactData._id}" onclick="update(this)" alt="edit" src="../assets/create-black-18dp.svg">
+            </td>
+        </tr>
+        `;
+    }
+    document.querySelector("#table-display").innerHTML = innerHtml;
+};
