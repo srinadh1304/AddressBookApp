1f0206445f23c90fc46350a618f4c2d25e86219a
srinadh1304 <srinadh1304@gmail.com>
2021-10-20 23:20:37 +0530

    ADDED: use local storage and server url attribute

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,4 +1,6 @@
 let site_properties = {
+    use_local_storage: "true",
+    server_url: "http://localhost:3000/ContactDB/",
     home_page: "../pages/home.html",
-    address_book_form_page: "../pages/address_book_form.html"
+    add_contact_page: "../pages/address_book_form.html"
 };
